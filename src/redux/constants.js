2e6559fec6ded1fe701ca30337b3915eac74c22f
PrangonGhose <prangonghose@gmail.com>
2023-02-07 05:52:23 +0600

    Declare constants

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,2 +1,2 @@
-/* eslint-disable */
 export const GET_MISSION = 'space-traveller-s-hub/src/redux/mission/GET_MISSION';
+export const UPDATE_MEMBER = 'space-traveller-s-hub/src/redux/mission/UPDATE_MEMBER';
