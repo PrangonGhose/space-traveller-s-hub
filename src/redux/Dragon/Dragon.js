a98871a1a8a7b07efb2fbb788e1a7d8bdd51c3f6
HunterX-7 <fraquin7@gmail.com>
2023-02-07 12:29:49 -0500

    Add dragonReducer to fetch changes

--- src/redux/Dragon/Dragon.js
+++ src/redux/Dragon/Dragon.js
@@ -2,6 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const GET_DATA = "GET_DATA";
+const RESERVE_DRAGON = "RESERVE_DRAGON";
 const dragonUrl = "https://api.spacexdata.com/v4/dragons";
 
 const getDragonApi = createAsyncThunk(GET_DATA,
@@ -19,3 +20,31 @@ const getDragonApi = createAsyncThunk(GET_DATA,
         return dragonData;
     })
     );
+
+    const reserveDragon = (id) => {(
+        {
+            type: RESERVE_DRAGON,
+            payload: id,
+        }
+    )};
+    
+    const dragonReducer = (state = [], action) => {
+        switch (action.type) {
+            case `${GET_DATA}/fulfilled`:
+                return action.payload;
+            case RESERVE_DRAGON:
+                return state.map((dragon) => {
+                    if (dragon.id === action.payload) {
+                        return {
+                            ...dragon, reserved: !dragon.reserved,
+                        }
+                    } return dragon;
+                });
+            default:
+                return state;
+        }
+    };
+    
+    export { getDragonApi, reserveDragon };
+    export default dragonReducer;
+    
