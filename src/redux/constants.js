d6d4c3fc4ffa2cdced13721fd2db36a811c331b0
PrangonGhose <prangonghose@gmail.com>
2023-02-07 16:03:09 +0600

    Add new action key

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,2 +1,3 @@
 export const GET_MISSION = 'space-traveller-s-hub/src/redux/mission/GET_MISSION';
 export const UPDATE_MEMBER = 'space-traveller-s-hub/src/redux/mission/UPDATE_MEMBER';
+export const GET_STORAGE = 'space-traveller-s-hub/src/redux/mission/GET_STORAGE';
