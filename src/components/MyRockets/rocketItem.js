32b52fbc4825f35d2c15193e514f00ced801be0b
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-02-07 16:19:23 -0600

    add all the rockets and the display

---
+++ src/components/MyRockets/rocketItem.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { ReserveRocket } from '../../redux/Rocket/Rocket';
+
+const RocketItem = (props) => {
+  const {
+    id, name, description, flickrImage, reserved,
+  } = props;
+
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    dispatch(ReserveRocket(id));
+  };
+
+  return (
+    <div>
+      <li>
+        <img src={flickrImage} alt="Girl in a jacket" width="250" height="250" />
+        <div>
+          <h2>{name}</h2>
+          <div>
+            <span className={reserved ? 'active' : 'inactive'}>
+              reserved
+            </span>
+            <p>{description}</p>
+          </div>
+          <button type="button" id={id} onClick={onClick} className={reserved ? 'reserve' : 'cancel'}>{reserved ? 'Cancel Reservation' : 'Reserve Rocket'}</button>
+        </div>
+      </li>
+    </div>
+  );
+};
+
+export default RocketItem;
+
+RocketItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  reserved: PropTypes.string.isRequired,
+  flickrImage: PropTypes.string.isRequired,
+};
