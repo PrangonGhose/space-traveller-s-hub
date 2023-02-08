fe3d1c27c47082cfac4978888fb317f8fb839d5c
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-02-08 09:12:19 -0600

    add the css and the reserve part

---
+++ src/components/RocketProfile.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// const RocketProfile = (props) => {
+//   const {
+//     name,
+//   } = props;
+
+//   return (
+//     <li>
+//       <h2>{name}</h2>
+//     </li>
+
+//   );
+// };
+
+// export default RocketProfile;
+
+// RocketProfile.propTypes = {
+//   name: PropTypes.string.isRequired,
+// };
