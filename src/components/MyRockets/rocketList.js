37bbcba015a34c0fcd969ae365fa7dbc0388f584
PrangonGhose <prangonghose@gmail.com>
2023-02-08 22:35:31 +0600

    Dispatch API call after initialization

--- src/components/MyRockets/rocketList.js
+++ src/components/MyRockets/rocketList.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getApiRockets } from '../../redux/Rocket/Rocket';
 import RocketItem from './rocketItem';
 
 const RocketsList = () => {
   const rockets = useSelector((state) => state.Rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getApiRockets());
+  }, [dispatch]);
 
   return (
     <div className="container">
